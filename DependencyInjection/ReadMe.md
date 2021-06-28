e1a6a10459167d8953f301a80110b5df39485ca5
VenkataRN <Venkat.RN@gmail.com>
2021-06-28 20:43:08 +0100

    Initial Code for Dependency Injection

---
+++ DependencyInjection/ReadMe.md
@@ -0,0 +1,15 @@
+Error Observed:
+
+
+***************************
+APPLICATION FAILED TO START
+***************************
+
+Description:
+
+Parameter 0 of constructor in com.example.dependencyInjection.Customers required a bean of type 'int' that could not be found.
+
+
+Action:
+
+Consider defining a bean of type 'int' in your configuration.
---
+++ DependencyInjection/src/main/java/com/example/dependencyInjection/Customers.java
@@ -0,0 +1,46 @@
+package com.example.dependencyInjection;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Customers {
+	private int custid;
+	private String custName;
+	private String custCourse;
+	
+	public Customers(int custid, String custName, String custCourse) {
+		super();
+		this.custid = custid;
+		this.custName = custName;
+		this.custCourse = custCourse;
+	}
+	public int getCustid() {
+		return custid;
+	}
+	public void setCustid(int custid) {
+		this.custid = custid;
+	}
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+	public String getCustCourse() {
+		return custCourse;
+	}
+	public void setCustCourse(String custCourse) {
+		this.custCourse = custCourse;
+	}
+
+//	public void displayCourseName() {
+//		System.out.print("Printing Customers CourseName");	
+//	}
+	public void displayCourseName() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
