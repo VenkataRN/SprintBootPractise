246b321f321f6e92c12034eb39ffcc49ee5ddc71
VenkataRN <Venkat.RN@gmail.com>
2021-09-30 23:32:52 +0100

    Base Project with Java 8

---
+++ schoolgrading/src/main/java/com/example/schoolgrading/SchoolgradingApplication.java
@@ -0,0 +1,14 @@
+package com.example.schoolgrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolgradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolgradingApplication.class, args);
+		System.out.println("Welcome To School Grading Spring Boot Application");
+	}
+
+}
---
+++ schoolgrading/src/test/java/com/example/schoolgrading/SchoolgradingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schoolgrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolgradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
